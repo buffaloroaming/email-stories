9a8edd986cfbd0463119395b07652c8fc5ea036a
r_dbl_l <iamgrimus@gmail.com>
2024-03-19 23:30:49 -0700

    add text content and space indicators for grid columns

--- src/stories/themeLayoutGrids/ThemeLayoutGrids.stories.js
+++ src/stories/themeLayoutGrids/ThemeLayoutGrids.stories.js
@@ -17,26 +17,36 @@ export default {
 
 export const FullWidth = {
   args: {
-    numOfColumns: 0
+    numOfColumns: 0,
+    contentWidth: '',
+    columnLayout: ''
   }
 }
 export const ColumnOf1 = {
   args: {
-    numOfColumns: 1
+    numOfColumns: 1,
+    contentWidth: 'Content Width: 600px',
+    columnLayout: [100, 600, 100]
   }
 }
 export const ColumnOf2 = {
   args: {
-    numOfColumns: 2
+    numOfColumns: 2,
+    contentWidth: '2-Column Content Width: 600px',
+    columnLayout: [290, 20, 290]
   }
 }
 export const ColumnOf3 = {
   args: {
-    numOfColumns: 3
+    numOfColumns: 3,
+    contentWidth: '3-Column Content Width: 600px',
+    columnLayout: [20, 174, 20, 172, 20, 174, 20]
   }
 }
 export const ColumnOf4 = {
   args: {
-    numOfColumns: 4
+    numOfColumns: 4,
+    contentWidth: '4-Column Content Width: 600px',
+    columnLayout: [20, 125, 20, 125, 20, 125, 20, 125, 20]
   }
 }
--- src/stories/themeLayoutGrids/ThemeLayoutGrids.vue
+++ src/stories/themeLayoutGrids/ThemeLayoutGrids.vue
@@ -1,53 +1,69 @@
 <template>
-  <section class="grid-container">
-    <div class="column-container">
-      <template v-if="numOfColumns === 0 || numOfColumns === 1">
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
+  <section :class="classes">
+    <p>
+      <span class="template-width">Full Width: 800px</span><br />
+      {{ contentWidth }}<br />
+      <template v-for="(colWidth, i) in columnLayout" :key="i">
+        <span>{{ `${colWidth}px` }}</span>
+        {{ i !== columnLayout.length - 1 ? `| ` : '' }}
       </template>
-      <template v-if="numOfColumns === 2">
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-      </template>
-      <template v-if="numOfColumns === 3">
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-      </template>
-      <template v-if="numOfColumns === 4">
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-        <div :class="classes">
-          {{ numOfColumns }}
-        </div>
-        <div class="column-spacer"></div>
-      </template>
-    </div>
+    </p>
+    <hr />
+    <section class="grid-container">
+      <div v-if="numOfColumns === 0" :class="classes">
+        {{ numOfColumns }}
+      </div>
+      <div v-else class="column-container">
+        <template v-if="numOfColumns === 1">
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+        </template>
+        <template v-if="numOfColumns === 2">
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+        </template>
+        <template v-if="numOfColumns === 3">
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+        </template>
+        <template v-if="numOfColumns === 4">
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+          <div :class="classes">
+            {{ numOfColumns }}
+          </div>
+          <div class="column-spacer"></div>
+        </template>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -55,7 +71,7 @@
 import { reactive, computed } from 'vue'
 
 export default {
-  props: ['numOfColumns'],
+  props: ['numOfColumns', 'contentWidth', 'columnLayout'],
   setup(props) {
     props = reactive(props)
     return {
