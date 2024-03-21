d863b9c89b577dda52c091055d4985d5647fbc52
r_dbl_l <iamgrimus@gmail.com>
2024-03-20 21:48:50 -0700

    add Iconography section and social icons

---
+++ src/stories/themeIconography/ThemeIconography.stories.js
@@ -0,0 +1,47 @@
+import ThemeIconography from './ThemeIconography.vue'
+
+export default {
+  title: 'Theme/Iconography',
+  component: ThemeIconography
+}
+
+export const Telephone = {
+  args: {
+    images: ['telephone_white.png', 'telephone_black.png']
+  }
+}
+export const Youtube = {
+  args: {
+    images: ['youtube_white.png', 'youtube_black.png']
+  }
+}
+export const HelloEmail = {
+  args: {
+    images: ['hello_white.png', 'hello_black.png']
+  }
+}
+export const Facebook = {
+  args: {
+    images: ['facebook_white.png', 'facebook_black.png']
+  }
+}
+export const Twitter = {
+  args: {
+    images: ['twitter_white.png', 'twitter_black.png']
+  }
+}
+export const LinkedInIcons = {
+  args: {
+    images: ['linkedIn_white.png', 'linkedIn_black.png']
+  }
+}
+export const Instagram = {
+  args: {
+    images: ['instagram_white.png', 'instagram_black.png']
+  }
+}
+export const HolaEmail = {
+  args: {
+    images: ['hola_white.png', 'hola_black.png']
+  }
+}
---
+++ src/stories/themeIconography/ThemeIconography.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div>
+      <figure v-for="(img, i) in images" :key="i">
+        <img :src="getImageURL(img)" />
+      </figure>
+    </div>
+  </section>
+</template>
+
+<script setup>
+function getImageURL(name) {
+  return new URL(`../../assets/images/iconography/${name}`, import.meta.url).href
+}
+defineProps({
+  images: []
+})
+</script>
+
+<style scoped>
+@import './ThemeIconography.css';
+</style>
