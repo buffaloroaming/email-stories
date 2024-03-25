505e3a0bc16114837d7a02b311315d66bc815f36
vincaswanson <vswanson@shawscott.com>
2024-03-25 14:00:21 -0700

    logo theme, logo images

---
+++ src/stories/themeLogos/ThemeLogos.vue
@@ -0,0 +1,25 @@
+<template>
+<section>
+    <div>
+      <figure v-for="(img, i) in images" :key="i">
+        <img :src="getImageURL(img)" :style="{width:size,margin:'.5em'}" />
+        <figcaption>{{caption}}</figcaption>
+      </figure>
+      
+    </div>
+  </section>
+</template>
+
+<script setup>
+function getImageURL(name) {
+  return new URL(`../../assets/images/logos/${name}`, import.meta.url).href
+}
+defineProps({
+    images: [], //defines here that imgPath is a string/array value, you don't need quotations
+    size: String,
+    caption: String
+})
+</script>
+
+
+
