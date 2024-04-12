18867b4d176cce5d4fcd2b4d8907caf9c58f514b
r_dbl_l <iamgrimus@gmail.com>
2024-04-11 19:38:06 -0700

    wip

--- src/stories/twoColumnModule/TwoColumnModuleCode.js
+++ src/stories/twoColumnModule/TwoColumnModuleCode.js
@@ -1,25 +1,41 @@
-export const TwoColumnModuleCode = `<!--hero left aligned -->
-<table role="presentation" style="width:100%;border:0;border-spacing:0;max-width:800px;background-color:#000001;" class="bg-light">
+export const TwoColumnModuleCode = `
+<!-- 24px spacer -->
+<i style="letter-spacing:50px;mso-font-width:-100%;display:inline-block;width:50px;font-size:0px;mso-text-raise:24px;height:24px;">&#8202;</i>
+<!-- /spacer -->
+<table role="presentation" align="center" style="width:75%;border:0;border-spacing:0;border-collapse: separate" class="w93">
 	<tr>
-		<td align="center" style="padding:40px 0px;">
-			<table role="presentation" style="width:75%;border:0;border-spacing:0;" class="w93">
+		<td style="border: 1px solid #000001;border-radius: 16px;" class="border">
+			<table role="presentation" align="center" style="width:93%;border:0;border-spacing:0;" class="w85">
 				<tr>
-					<td><h1 style="color:#fdfeff" class="dmTextBlk">This is a headline and it is so <span class="green">freaking amazing</span>, so we used it in this template</h1></td>
-				</tr>
-        <tr>
-					<td style="padding-top:20px;">
-            <p style="color:#fdfeff" class="dmTextBlk">Current balance: {{numberFormat cardCurrentBalance "currency" "en_US"}}</p>
-					</td>
-				</tr>
-				<tr>
-					<td style="padding-top:20px;">
-					<!-- cta white mbl width-->
-					<a href="https://oportun.com" style="font-size: 17px; line-height: 22px;font-weight: 500;color:#000001;background-color: #fdfeff;text-underline-color:#fdfeff;text-align: center;text-decoration: none; padding: 12px 40px; border-radius:15px; display:inline-block; mso-padding-alt:0;color:#000001;" class="btnWht mblBtn"><!--[if mso]><i style="letter-spacing:40px;mso-font-width:-100%;mso-text-raise:22pt;">&nbsp;</i><![endif]--><span style="mso-text-raise:12pt;">CTA White</span><!--[if mso]><i style="letter-spacing: 40px;mso-font-width:-100%">&nbsp;</i><![endif]--></a>
-					<!-- /cta white mbl width-->
+					<td align="center" style="font-size:0;padding:12px 0px;">
+					<!--[if mso]><table role="presentation" width="100%"><tr><td style="width:316px;vertical-align:middle" align="center"><![endif]-->
+					<div style="width:100%;max-width:316px;display:inline-block;vertical-align:middle;" class="mw100">
+					<table role="presentation" align="center" style="width:100%;border:0;border-spacing:0;">
+						<tr>
+							<td style="padding-top:12px;padding-right:20px;" class="clrLR">
+							<h2 style="color:#000001" class="dmTextWht">Paga a tiempo, todo el tiempo</h3>
+							<p style="margin-top:20px;" class="dmTextWht">Establecer pagos automáticos es la manera más fácil de asegurarte que tus pagos se hagan a tiempo. Puedes crear pagos automáticos en tu cuenta en línea.</p>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding:20px 20px 12px 0px;" class="clrLR">
+							<!-- cta black-->
+							<a href="https://account.oportun.com/creditcards" style="font-size: 17px; line-height: 22px; font-weight: 500;color:#fdfeff;background-color:#000001;text-underline-color:#000001;text-align: center;text-decoration: none; padding: 12px 40px; border-radius:15px; display:inline-block; mso-padding-alt:0;color:#fdfeff" class="btnBlk"><!--[if mso]><i style="letter-spacing:40px;mso-font-width:-100%;mso-text-raise:22pt;">&nbsp;</i><![endif]--><span style="mso-text-raise:12pt;">Establecer pagos automáticos</span><!--[if mso]><i style="letter-spacing: 40px;mso-font-width:-100%">&nbsp;</i><![endif]--></a><!-- /cta black-->
+							</td>
+						</tr>
+					</table>
+					</div><!--[if mso]></td><td style="width:224px;vertical-align:middle" align="center"><![endif]-->
+					<div style="width:100%;max-width:224px;display:inline-block;vertical-align:middle;" class="mw100">
+					<table role="presentation" align="center" style="width:100%;border:0;border-spacing:0;">
+						<tr>
+							<td align="center" class="mblPad"><img src="https://res.cloudinary.com/oportun/image/upload/v1675879898/email/illustrations/desktop/email_SignupAutopaySquare.png" width="224" alt="" style="display:block;width:100%;max-width:480px;height:auto;" /></td>
+						</tr>
+					</table>
+					</div><!--[if mso]></td></tr></table><![endif]-->
 					</td>
 				</tr>
 			</table>
 		</td>
 	</tr>
 </table>
-<!-- /hero left aligned -->`
+`
